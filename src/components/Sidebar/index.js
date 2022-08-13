74ccc9f6daaa0fffdff03ceb30088e5fcc8d97c1
UdaySaiGandra <udaysaigandra@gmail.com>
2022-08-13 17:22:30 -0400

    homepage banner added

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,8 +3,8 @@ import './index.scss';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faEnvelope, faHome, faUser,} from '@fortawesome/free-solid-svg-icons'
-
+import {faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
     return <div className='nav-bar'>
@@ -23,6 +23,27 @@ const Sidebar = () => {
                       <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                 </NavLink>
              </nav>
+
+             <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/uday-g/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/udaygandra"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
 }
 
